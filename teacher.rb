69e5516f49fc97a24c0622c13b8c20a1aab397ae
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-08-27 13:24:34 +0300

    Set up consctructur that includes the new property

--- teacher.rb
+++ teacher.rb
@@ -1,3 +1,6 @@
 require_relative 'person'
-
 class Teacher < Person
+    initialize(id, name = 'unknown', age, parent_permission= true, specialization)
+    super(id, name, age, parent_permission,)
+    @specialization = specialization
+end
