4f708ec4c11089aadf6625203430aa8527665af6
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-08-29 00:09:55 +0300

    Move decorator files into a separate directory

--- nameable.rb
+++
@@ -1,5 +0,0 @@
-class Nameable
-  def correct_name
-    raise NotImplementedError, 'Subclasses must implement this method'
-  end
-end
--- output.rb
+++
@@ -1,9 +0,0 @@
-require_relative 'person'
-require_relative 'decorator'
-require_relative 'trimmer_decorator'
-
-person4 = Person.new(22, 'maximilianus')
-decorated_person = Decorator.new(person4) # capitalize the first letter
-trimmed_person = Trimmer.new(decorated_person) # trim the name
-
-puts trimmed_person.correct_name
