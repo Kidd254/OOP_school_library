f61e1d21d3e579cfe3c1dc594ea0f37c27b1f981
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-08-29 21:47:00 +0300

    Create has many relationship in class Classroom

--- associations/classroom.rb
+++ associations/classroom.rb
@@ -1,6 +1,13 @@
+require_relative '../student'
+
 class Classroom
-    attr_accessor :label
-    def initialize(label)
+    attr_accessor :label, :students
+    def initialize(label, students: [])
         @label = label
+        @students = students
     end
+
+    def add_student(student)
+        @student << student
+        student.classroom = self
 end
