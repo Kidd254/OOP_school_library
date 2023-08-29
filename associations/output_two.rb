6371c05d41d6691504f3a10212c945a82b021da6
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-08-29 23:54:18 +0300

    Add test cases

---
+++ associations/output_two.rb
@@ -0,0 +1,12 @@
+require 'date'
+require_relative 'rental'
+require_relative 'book'
+require_relative '../person'
+
+book = Book.new("The Great Gatsby", "F. Scott Fitzgerald")
+person5 = Person.new("John Kidd", 26)
+rental = Rental.new(Date.new(2023, 8, 1), book, person5)
+
+puts "Rental Date: #{rental.date}"
+puts "Rented Book Title: #{book.title}"
+puts "Rented by Person: #{person5.name}"
