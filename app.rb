d7133fd898e87db3f0086e70d2fc24be5bbed6ee
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-08-30 13:31:28 +0300

    List all books, people and rentals, create person, book and rental

---
+++ app.rb
@@ -0,0 +1,66 @@
+require_relative 'person'
+require_relative 'student'
+require_relative 'teacher'
+require_relative 'associations/book'
+class App
+  def initialize(books: [], people: [], rental: [])
+    @books = books
+    @people = people
+    @rental = rental
+  end
+
+  def list_books
+    @books.each { |book| puts book.title }
+  end
+
+  def list_people
+    @people.each { |person| puts person.name }
+  end
+
+  def create_person(role)
+    puts 'Enter name:'
+    name = gets.chomp
+
+    puts 'Enter age:'
+    age = gets.chomp.to_i
+
+    if role == 'student'
+      puts 'Enter parent permission (true/false):'
+      parent_permission = gets.chomp.downcase == 'true'
+      person = Student.new(age, name, parent_permission: parent_permission)
+    elsif role == 'teacher'
+      puts 'Enter specialty:'
+      specialty = gets.chomp
+      person = Teacher.new(age, name, specialty: specialty)
+    else
+      puts 'Invalid role'
+      return
+    end
+
+    @people << person
+    puts "#{role.capitalize} '#{name}' created."
+  end
+
+  def create_book
+    puts 'Enter title:'
+    title = gets.chomp
+
+    puts 'Enter author:'
+    author = gets.chomp
+
+    book = Book.new(title, author)
+    @books << book
+
+    puts "Book '#{title}' by #{author} created."
+  end
+
+  def list_rentals_for_person(person_id)
+    person = @people.find { |p| p.id == person_id }
+    return unless person
+
+    person.rentals.each do |rental|
+      book = @books.find { |b| b.id == rental.book_id }
+      puts "Book: #{book.title}, Date: #{rental.date}"
+    end
+  end
+end
