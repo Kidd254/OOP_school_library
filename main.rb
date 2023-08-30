b61bc08f4cfe665466a3fa24cabad69f87f9d3cb
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-08-30 18:11:38 +0300

    Add an entry point to perform various tasks

---
+++ main.rb
@@ -0,0 +1,65 @@
+require_relative 'person'
+require_relative 'student'
+require_relative 'teacher'
+require_relative 'associations/book'
+require_relative 'app'
+
+
+app = App.new
+
+loop do
+  puts 'Choose an option:'
+  puts '1. List all books'
+  puts '2. List all people'
+  puts '3. Create a person'
+  puts '4. Create a book'
+  puts '5. Create a rental'
+  puts '6. List all rentals for a person'
+  puts '7. Quit'
+
+  choice = gets.chomp.to_i
+
+  case choice
+  when 1
+    app.list_books
+    p app.instance_variable_get(:@books) 
+  when 2
+    app.list_people
+    p app.instance_variable_get(:@people)  # Print people array
+  when 3
+    puts 'Enter role (student/teacher):'
+    role = gets.chomp.downcase
+    app.create_person(role)
+  when 4
+    app.create_book
+    p app.instance_variable_get(:@books)  # Print books array
+  when 5
+    puts 'Enter person ID:'
+    person_id = gets.chomp.to_i
+    person = app.instance_variable_get(:@people).find { |p| p.id == person_id }
+
+    if person.nil?
+      puts 'Person not found.'
+      next
+    end
+    puts 'Enter book ID:'
+    book_id = gets.chomp.to_i
+    book = app.instance_variable_get(:@books).find { |b| b.id == book_id }
+
+    if book.nil?
+      puts 'Book not found.'
+      next
+    end
+    app.create_rental(person_id, book_id)
+  when 6
+    puts 'Enter person ID:'
+    person_id = gets.chomp.to_i
+    app.list_rentals_for_person(person_id)
+  when 7
+    puts 'Goodbye!'
+    break
+  else
+    puts 'Invalid option. Please choose again.'
+  end
+  gets.chomp
+end
