76fa151357ced234c65869d400b28498b169e464
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-08-30 18:12:51 +0300

    Delete unwanted instance

--- teacher.rb
+++ teacher.rb
@@ -1,8 +1,9 @@
 require_relative 'person'
+
 class Teacher < Person
   attr_accessor :specialization
 
-  def initialize(age, specialization, name = 'unknown', parent_permission: true)
+  def initialize(age, name = 'unknown', parent_permission: true, specialization: nil)
     super(age, name, parent_permission: parent_permission)
     @specialization = specialization
   end
@@ -11,14 +12,3 @@ class Teacher < Person
     true
   end
 end
-
-# Create a Teacher instance
-teacher = Teacher.new(3, 35, 'Mathematics')
-
-# Output attributes using getters
-puts "Teacher ID: #{teacher.id}"
-puts "Teacher Age: #{teacher.age}"
-puts "Teacher Specialization: #{teacher.specialization}"
-
-# Test can_use_services? method
-puts "Teacher can use services: #{teacher.can_use_services?}"
