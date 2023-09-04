173166b77affa31696673ea10260eecd2d2eadd2
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-09-04 22:04:15 +0300

    Refactor main.rb

--- main.rb
+++ main.rb
@@ -1,11 +1,12 @@
+require 'pry'
+require_relative 'app'
 require_relative 'person'
 require_relative 'student'
 require_relative 'teacher'
 require_relative 'associations/book'
-require_relative 'app'
 
 app = App.new
-# rubocop:disable Metrics/BlockLength
+
 loop do
   puts 'Choose an option:'
   puts '1. List all books'
@@ -24,34 +25,13 @@ loop do
   when 2
     app.list_people
   when 3
-    puts 'Enter role (student/teacher):'
-    role = gets.chomp.downcase
-    app.create_person(role)
+    app.create_person_prompt
   when 4
-    app.create_book
-    app.instance_variable_get(:@books) # Print books array
+    app.create_book_prompt
   when 5
-    puts 'Enter person ID:'
-    person_id = gets.chomp.to_i
-    person = app.instance_variable_get(:@people).find { |p| p.id == person_id }
-
-    if person.nil?
-      puts 'Person not found.'
-      next
-    end
-    puts 'Enter book ID:'
-    book_id = gets.chomp.to_i
-    book = app.instance_variable_get(:@books).find { |b| b.id == book_id }
-
-    if book.nil?
-      puts 'Book not found.'
-      next
-    end
-    app.create_rental(person_id, book_id)
+    app.create_rental_prompt
   when 6
-    puts 'Enter person ID:'
-    person_id = gets.chomp.to_i
-    app.list_rentals_for_person(person_id)
+    app.list_rentals_for_person_prompt
   when 7
     puts 'Goodbye!'
     break
@@ -59,4 +39,3 @@ loop do
     puts 'Invalid option. Please choose again.'
   end
 end
-# rubocop:enable Metrics/BlockLength
