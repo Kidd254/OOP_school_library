7a528aa582c31e3e5547461492eabe44f2fa3c4b
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-09-06 21:55:38 +0300

    Fix a bug identified through tests

--- person.rb
+++ person.rb
@@ -1,7 +1,7 @@
 require_relative 'ruby_files/nameable'
 
 class Person < Nameable
-  attr_accessor :name, :age, :rentals
+  attr_accessor :name, :age, :rentals, :parent_permission
   attr_reader :id
 
   def initialize(age, name = 'unknown', parent_permission: true)
